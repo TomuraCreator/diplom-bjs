418acdf7f341dc1e0405aa70ce45afd7483bb344
tery <vladislavutkin92@gmail.com>
2019-07-25 23:52:46 +0300

    частично реальзована авторизация

--- static/main.js
+++ static/main.js
@@ -1,44 +1,49 @@
 class Profile {
-    constructor({
-        username,
-        name: { firstName, lastName },
-        password,
-    }) {
-        this.username = username,
-        this.name = {firstName: firstName, lastName: lastName},      
-        this.password = password
+    constructor(obj) 
+    {
+        this.username = obj.username,
+        this.name = {firstName: obj.name.firstName, lastName:  obj.name.lastName},      
+        this.password = obj.password
+    } 
+    addNewPerson() {
+        console.log('Создаём пользователя.')
+        ApiConnector.createUser({username: this.username,
+        name: this.name,
+        password: this.password }, (err, data) => {
+            
+            if (err) throw err;
+            this.autorizationNewPerson();
+            console.log('Авторизация...')
+
+        })
+    }
+    autorizationNewPerson() {
+        ApiConnector.performLogin ({username: this.username, password: this.password}, (err, data) => {
+            if (err) throw err;
+            console.log("Авторизация прошла успешно");
+            
+            
+        });
+            
+    } 
+    addMoney({valute, amount}) {
 
     }
 }
 
-// function getExchangeRates() {
-//     const exchangeValute = ApiConnector.getStocks((err, data) => {
-//         if (err) {
-//             console.error('Error during adding money to Ivan');
-//         } else {
-//             return data[0];
-//         }
+function getStocks(num ,callback) {
+    ApiConnector.getStocks((err, data) => {
+        console.log(`Getting stocks info`);
+        array.push(callback(err, data[num]));
+    });
+    return array.map((currentValue) => currentValue)
+}
 
-//     });
-//     console.log(exchangeValute);
-// }
 const newPerson = new Profile({
-    username: 'werwe',
-    name: { firstName: 'Влад', lastName: 'Уткин' },
-    password: '123123'
+    username: 'Tomura',
+    name: { firstName: 'Влад', lastName: 'Уткин' }, password: 'qwerty'
 })
+// newPerson.addNewPerson();
+newPerson.addNewPerson(newPerson.autorizationNewPerson);
 
-function callbackFunction() {
-    
-    return (err, data) => {
-        if (err) {
-            console.log(error)
-        } else {
-            console.log(data)
-        }
-    }
-}
-
-
-// console.log(ApiConnector.getStocks(callback(err, data)))
-console.log(ApiConnector.createUser(newPerson, callbackFunction('Всё очень плохо', 'Выполнение запроса')))
+// console.dir(newPerson)
