79bf0932f9bf40ea814e3ea5085c1baffb86b9eb
tery <vladislavutkin92@gmail.com>
2019-07-29 17:46:39 +0300

    убраны промежуточные переменные, добавлен цветовой вывод,

--- static/main.js
+++ static/main.js
@@ -5,58 +5,58 @@ class Profile {
         this.username = obj.username,
         this.name = {firstName: obj.name.firstName, lastName:  obj.name.lastName},      
         this.password = obj.password
-    } 
+    }
     createUser(callback) {
-        console.log(`Create user ${this.username}`)
+        console.log(`Create user ${this.username} (¬‿¬) \n %c...`, `color: 	#CC6600`);
         ApiConnector.createUser({username: this.username,
         name: this.name,
         password: this.password }, (err, data) => {
+            console.log(`%c User ${this.username} created succesfully!(─‿‿─)`, `background: #000000; color: 	#CCFFCC;`);
             callback(err,data)
-        })
+        });
     }
     performLogin(callback) {
-        console.log(`Authorizing user ${this.username}`)
+        console.log(`Authorizing user ${this.username} (◕‿◕) \n %c...`, `color: #CC6600`)
         ApiConnector.performLogin ({username: this.username, password: this.password}, (err, data) => {
-            console.log(`${this.username} is authirized`);
+            console.log(`%c ${this.username} authirized successfully! (─‿‿─)`, `background: #000000; color: #CCFFCC;`);
             callback(err, data);
-        });
-            
+        });      
     } 
     addMoney({currency, amount}, callback) {
-        
+        console.log(`We add ${amount + currency} to the account. (；一_一) \n %c...`, `color: #CC6600`);
         ApiConnector.addMoney({currency, amount}, (err, data) => {
-            if(err) console.error(`Error during adding money`);
-            console.log(`Added ${amount + currency}`);
+            if(err) throw err;
+            console.log(`%c Great! The operation was successful (づ￣ ³￣)づ`, `background: #000000; color: 	#CCFFCC;` )
             callback(err, data)
         })
     }
     convertValute({fromCurrency, targetCurrency, targetAmount}, callback) {
+        console.log(`Money converted to netcoin (❍ᴥ❍ʋ) \n %c...` , `color: #CC6600`)
         ApiConnector.convertMoney({fromCurrency, targetCurrency, targetAmount}, (err, data) => {
-            console.log(`Money converted to netcoin`)
+            console.log(`%c Conversion successfully completed (ᵔᴥᵔ) `, `background: #000000; color: 	#CCFFCC;`)
             callback(err, data)
-            console.log(`Added money`)
         })
     }
     transferMoney({to, amount}, callback) {
+        console.log(`Transfer funds, please wait ◉_◉ \n %c...` , `color: #CC6600`)
         ApiConnector.transferMoney({to, amount}, (err, data)=> {
-            
             callback(err, data)
+            console.log(`%cMoney transfered succesfully ʕ•ᴥ•ʔ`, `background: #000000; color: 	#CCFFCC;`);
         })
     }
 }
-
 
 function getStocks(callback) {
     ApiConnector.getStocks((err, data) => {
         if(err) throw err;
         callback(err, data);
     }); 
-} // 
+} 
 
 function main() {
     const NewPerson = new Profile({
         username: 'Tomura',
-        name: { firstName: 'Влад', lastName: 'Уткин' }, password: 'qwerty'
+        name: {firstName: 'Влад', lastName: 'Уткин'}, password: 'qwerty'
     })
     const FriendPerson = new Profile({
         username: 'koresh',
@@ -64,27 +64,22 @@ function main() {
     })
     getStocks((err, data) => {
         if(err) throw err;
-        let array1 = [];
-        let log = [];
-        data.forEach((element, array) => {
-            log.push(element.EUR_NETCOIN);
-        })
-        array1.push((log.sort((a,b)=> a-b))[0]) // только выгодный курс в нашем банке (работаем в убыток - цените!) 
-        const values = {currency: 'EUR', amount: 123456}
+        let array1 = [{currency: 'EUR', amount: 123456}];
+        data.forEach((element, array) => array1.push(element.EUR_NETCOIN));
+        array1.push((array1.sort((a,b)=> a-b))[1]); // только выгодный курс в нашем банке (работаем в убыток - цените!)
             NewPerson.createUser((err, data) => {
                 if(err) throw err;
                 NewPerson.performLogin((err, data) => {
                     if(err) throw err;
-                    NewPerson.addMoney(values, (err, data)=> {
+                    NewPerson.addMoney(array1[0], (err, data)=> {
                         if(err) throw err;
-                        NewPerson.convertValute({fromCurrency: values.currency, targetCurrency: 'NETCOIN', targetAmount: Number(array1[0]) * values.amount}, (err, data)=> {
+                        NewPerson.convertValute({fromCurrency: array1[0].currency, targetCurrency: 'NETCOIN', targetAmount: Number(array1[1]) * array1[0].amount}, (err, data)=> {
                             if(err) throw err;
                             FriendPerson.createUser((err, data)=>{
                                 if(err) throw err;
                                     if(err) throw err;
                                     NewPerson.transferMoney({to: 'koresh', amount: 500}, (err, data)=> {
                                         if(err) throw err
-                                        console.log(`Money transfered`);
                                 })
                             })
                         })
